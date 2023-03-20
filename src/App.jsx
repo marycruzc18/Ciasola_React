1d9bd640cb9852fdffac1cf90a3af0a7cac89045
Mary Casasola <marycruzc18@gmail.com>
2023-03-19 22:17:17 -0300

    Entrega Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import ItemListContainer from './components/ItemListContainer'
-import Navbar from './components/NavBar'
+import React from "react";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Gracias from "./components/Gracias";
+import { CartContextProvider } from "./contexts/CartContext";
+import "./App.css";
 
-function App() {
+const  App = () => {
  
 
   return (
-    <>
-     <Navbar/>
-     <ItemListContainer greeting={"Bienvenidos a Ciasola"}/>
-    </>
-  )
-}
+  
+    <CartContextProvider>
 
-export default App
+        <BrowserRouter>
+
+        <Navbar/>
+
+        <Routes>
+
+          <Route exact path="/" element= {<ItemListContainer />} />
+
+          <Route exact path="/category/:category" element= {<ItemListContainer/>} />
+
+          <Route exact path="/item/:id" element= {<ItemDetailContainer/>} />
+
+          <Route exact path="/cart" element= {<Cart/>} />
+
+          <Route exact path={"/checkout"} element={<Checkout />} />
+
+          <Route exact path={"/gracias/:id"} element={<Gracias />} />
+
+        </Routes>
+        <Footer />
+        </BrowserRouter>
+
+    </CartContextProvider>
+  );
+};
+
+export default App;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import ItemList from "./ItemList";
+import { useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import{ collection, getDocs, getFirestore } from "firebase/firestore";
 
-const ItemListContainer = ({greeting}) => {
-    return(
-     
-        <section>
 
-            <p className='texto_principal'>{greeting}</p>
 
-        </section>
-
+const ItemListContainer = () => {
+    const[productos, setProductos] = useState([]);
+    const {category} = useParams();
     
+    useEffect(() => {
+      const db = getFirestore();
+      const productosCollection = collection (db, "productos");
+      getDocs(productosCollection).then ((querySnapshot) => {
+        const productos= querySnapshot.docs.map((doc) => ({
+        ...doc.data(), 
+        id:doc.id,
+        }));
+        setProductos(productos);
+      });
+   
+    }, []);
+
+
+    const filCate = productos.filter((producto)=> producto.category === category );
+
+    return (
+
+        <div>
+            {category ? <ItemList productos = {filCate} /> : <ItemList productos = {productos} />}
+        </div>
     );
 
-}
+};
+
+   
 
 export default ItemListContainer;
